d8a311aa3c547a55d474965bb5c72d450c073730
digvijaysingh3023 <digvijaysihag123@gmail.com>
2023-01-21 14:28:27 +0530

    chnge

--- PassPortal/settings.py
+++ PassPortal/settings.py
@@ -131,12 +131,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-    
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  
+]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 
 # Default primary key field type
