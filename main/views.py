3c23c00d4f163351ed05380e3b8a941fb70af755
digvijaysingh3023 <digvijaysihag123@gmail.com>
2023-01-22 05:24:25 +0530

    amount updated

--- main/views.py
+++ main/views.py
@@ -154,6 +154,7 @@ def SaveData(request):
         doc_ref = db.collection('users').document(id)
         doc_ref.set(Ldata)
         amount=paases_type['general']*500+(paases_type['premium']+paases_type['premium'])*750
+        amount=1
         print(amount)
         paases_type['amount']=amount
         data = id+"|"+fee_id+"|"+str(amount)
