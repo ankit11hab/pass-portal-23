8648b3434da5f9dd125417bb47f593802effaf57
ankit11hab <ankit11hab@gmail.com>
2023-01-21 14:44:44 +0530

    statuc

--- PassPortal/settings.py
+++ PassPortal/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  
 ]
