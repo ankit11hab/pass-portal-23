e0619ec3546bd6fef9aab0a7c7aac3c2d475d463
digvijaysingh3023 <digvijaysihag123@gmail.com>
2023-01-21 22:12:07 +0530

    mobile view register page

---
+++ static/js/scriptAddIndividual.js
@@ -0,0 +1,90 @@
+const addIndividual=document.querySelector(".addIndividualBtnDiv");
+const referenceDiv=document.querySelector(".form-bottom");
+let k=0;
+// const number_arr=["Second","Third","Fourth","Fifth","Sixth","Seventh","eighth","ninth","eleventh"];
+const removeButton=document.querySelector("#removeBtn-1");
+
+
+addIndividual.addEventListener('click',target=>{
+    console.log("hi");
+    referenceDiv.insertAdjacentHTML('beforebegin',`<div class="form-mid" id="extraDiv${k+1}">
+    <!-- <img class="bgpic" src="./form mid.png" alt=""> -->
+    <div class="form">
+        <div class="person">
+            <div class="pass-type">
+                <P class="p-person"> Member</P>
+                <div class="remove-button" id="removeBtn-${k+1}" onclick="Delete(event, 'extraDiv${k+1}')">
+                    Remove
+                </div>
+                <p class="textinput">Pass type</p>
+                <div class="custom-select">
+                    <select name="pass_type" id="" onchange=onchnge_()>
+                    <option value="none" disabled selected hidden>Select</option>
+                    <option value="general"> General
+                    </option>
+                    <option value="premium"> Premium
+                    </option>
+                    <option value="exclusive"> Exclusive
+                    </option>
+                </select>
+                </div>
+            </div>
+            <div class=" pass-type">
+                <p class="textinput"> First Name</p>
+                <input type="text" class="input-text-field " name="first_name" required>
+            </div>
+            <div class=" pass-type">
+                <p class="textinput"> Last Name</p>
+                <input type="text" class="input-text-field " name="last_name" required>
+            </div>
+            <div class="pass-type">
+                <p class="textinput">E-mail</p>
+                <input type="text" class="input-text-field " name="email" required>
+            </div>
+            <div class=" pass-type">
+                <p class="textinput">Phone Number</p>
+                <input type="number" class="input-text-field " name="contact_no" required>
+            </div>
+            <div class="pass-type age-gen">
+                <div class="pass-gender">
+                    <p class="textinput">Gender</p>
+                    <div class="custom-select2">
+                        <select name="gender" id="">
+                        <option value="" disabled selected hidden ></option>
+  
+                    <option value="male"> Male
+                    </option>
+                    <option value="female"> Female
+                    </option>
+                    <option value="other"> Rather don't say
+                    </option>
+                    </select>
+                    </div>
+                </div>
+                <div class="pass-age">
+                    <p class="textinput">Age</p>
+                    <div class="custom-select3">
+                <input class="input-text-field" type='number' name='age'>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
+    </div>
+</div>`);
+k++;
+
+})
+
+// removeButton.addEventListener('click',target=>{
+//     console.log("remove");
+//     const extraDivision=document.querySelector("#extraDiv1");
+//     extraDivision.innerHTML=``;
+// });
+function Delete(event, id) {
+    onchnge_()
+    event.preventDefault();
+    var element = document.getElementById(id);
+    element.parentNode.removeChild(element);
+}
