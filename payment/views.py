f882b45ba5925da434f8144119ae04ddb40f534d
digvijaysingh3023 <digvijaysihag123@gmail.com>
2023-01-22 08:12:29 +0530

    chnge

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,5 @@
 import json
 from django.shortcuts import render, redirect
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from django.http import HttpResponse
 from django.http import JsonResponse
 from main.views import db
