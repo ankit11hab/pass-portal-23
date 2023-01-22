dc71a69110a1e1d60c804a09a927ff916c95f373
ankit11hab <ankit11hab@gmail.com>
2023-01-23 03:10:58 +0530

    update

--- payment/views.py
+++ payment/views.py
@@ -17,6 +17,7 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import io
 import os
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -124,13 +125,11 @@ def payment_response(request):
                 email = request.session.get('LeaderEmail')
                 count = request.session.get('count')
                 from_email = settings.EMAIL_HOST_USER
-                message = EmailMessage(
-                    'Confirmation Mail',
-                    'Your registeration for Alcheringa 2023 has been sent to us.Hang on to your cape and keep an eye out we will send you a QR code shortly',
-                    from_email,
-                    [email],
-                )
-                message.send()
+                subject = 'Confirmation Mail'
+                otp = random.randint(1000, 9999)
+                message = 'Your registeration for Alcheringa 2023 has been sent to us.Hang on to your cape and keep an eye out we will send you a QR code shortly'
+                from_email = settings.EMAIL_HOST_USER
+                send_mail(subject, message, from_email, [email])
             # generate_qr_code(request,leader_array,member_array)
             return redirect('get_payment_details')
         else:
