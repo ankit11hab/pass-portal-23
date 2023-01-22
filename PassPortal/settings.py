44f64d059c5e206075dc49cdb7707e6077ddccad
digvijaysingh3023 <digvijaysihag123@gmail.com>
2023-01-22 08:06:07 +0530

    updated requirements.txt

--- PassPortal/settings.py
+++ PassPortal/settings.py
@@ -135,7 +135,8 @@ MEDIA_URL = '/image_uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'image_uploads')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets')
 ]
 
 STATIC_ROOT = './static_files/'
