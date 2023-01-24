046f2121c7450a5ca2c69341efd95ef08fc66586
Akshat2403 <95174507+Akshat2403@users.noreply.github.com>
2023-01-24 20:57:34 +0530

    update models

--- main/views.py
+++ main/views.py
@@ -97,7 +97,7 @@ def SaveData(request):
         key = "Jkdh9rs6x1mSKH2lDFZ6z6057x4p8CL7"
         iv = "adjfytryd5g87hgh"
         id = ''.join(random.choices(string.ascii_uppercase +
-                                    string.digits, k=8))
+                                    string.digits, k=5))
         amount = 1
         fee_id = "M1006"
         print(id)
@@ -112,7 +112,7 @@ def SaveData(request):
         LeaderLastName = request.POST.get('LeaderLastName')
         LeaderContact_no = request.POST.get('LeaderContact_no')
         # LeaderEmail = request.POST.get('LeaderEmail')
-        LeaderEmail =         request.session['LeaderEmail']
+        LeaderEmail = request.session['LeaderEmail']
         print(LeaderEmail)
         LeaderPassType = request.POST.get('LeaderPassType')
         LeaderIDType = request.POST.get('LeaderIDtype')
@@ -135,7 +135,20 @@ def SaveData(request):
             paases_type['premium'] = paases_type['premium']+1
         elif (LeaderPassType == 'exclusive'):
             paases_type['exclusive'] = paases_type['exclusive']+1
+        Ldata = {
+            "LName": LeaderFirstName+' ' + LeaderLastName,
+            "LContact": LeaderContact_no,
+            "LEmail": LeaderEmail,
+            "LPassType": LeaderPassType,
+            "LIDType": LeaderIDType,
+            "LIDNumber": LeaderIDNumber,
+            "LAge": LeaderAge,
+            "LGender": LeaderGender,
+            # "members": members
+        }
 
+        doc_ref = db.collection('users').document(id)
+        doc_ref.set(Ldata)
         count = 1
         for i in member_first_names:
             count = count+1
@@ -153,6 +166,7 @@ def SaveData(request):
                 "gender": gender,
                 'email': email,
             }
+            doc_ref.collection('members').document().set(member)
             members.append(member)
             if (pass_type == 'general'):
                 paases_type['general'] = paases_type['general']+1
@@ -160,20 +174,7 @@ def SaveData(request):
                 paases_type['premium'] = paases_type['premium']+1
             elif (pass_type == 'exclusive'):
                 paases_type['exclusive'] = paases_type['exclusive']+1
-        Ldata = {
-            "LName": LeaderFirstName+' ' + LeaderLastName,
-            "LContact": LeaderContact_no,
-            "LEmail": LeaderEmail,
-            "LPassType": LeaderPassType,
-            "LIDType": LeaderIDType,
-            "LIDNumber": LeaderIDNumber,
-            "LAge": LeaderAge,
-            "LGender": LeaderGender,
-            "members": members
-        }
 
-        doc_ref = db.collection('users').document(id)
-        doc_ref.set(Ldata)
         amount = paases_type['general']*500 + \
             (paases_type['premium']+paases_type['premium'])*750
         amount = 1
